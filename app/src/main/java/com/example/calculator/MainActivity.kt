6894ed0eb19688b35d755aa903782653d6dcece5
GuiQRSM <endbeob15@gmail.com>
2021-07-31 18:08:07 -0300

    Implements the command to calculate the user's final average, and also displays the total number of faults

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalcular: Button = processar
+        val resposta: TextView = resposta
+
+        btCalcular.setOnClickListener {
+            val nota1: Int = Integer.parseInt(nota1.text.toString())
+            val nota2: Int = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2) / 2
+            val falta: Int = Integer.parseInt(faltas.text.toString())
+
+            if(media >= 6 && falta <= 10) {
+                resposta.setText("Situação final: APROVADO" + "\n" + "Valor da média:" + media + "\n" + "Total de faltas:" + faltas)
+            }else{
+                resposta.setText("Situação final: REPROVADO" + "\n" + "Valor da média:" + media + "\n" + "Total de faltas:" + faltas)
+            }
+        }
     }
 }
